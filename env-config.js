b72059820bac69bc6ee79b71ff6e4d52e52b5971
Sodiey <sotkarapo23@gmail.com>
2021-01-21 15:28:49 +0100

    fix routing on prod

--- env-config.js
+++ env-config.js
@@ -1,7 +1,5 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL': prod
-    ? 'https://github.com/Sodiey/next-color_JS'
-    : '',
+  'process.env.BACKEND_URL': prod ? '/next-color_JS' : '',
 };
