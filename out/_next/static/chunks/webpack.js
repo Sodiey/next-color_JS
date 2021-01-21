040529e91ef98e05946c62770df3486015c5a140
Sodiey <sotkarapo23@gmail.com>
2021-01-21 19:02:09 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c295850608a811708db8";
+/******/ 	var hotCurrentHash = "96e41aab49a3c3c480c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
